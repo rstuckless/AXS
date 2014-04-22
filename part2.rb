abf50f5ea347ed9960fa167d15e218edf86baa4d
Connor Poole <cpoole@usc.edu>
2014-04-22 15:43:46 -0700

    bug fixes

--- part2.rb
+++ part2.rb
@@ -8,14 +8,14 @@ browser= Watir::Browser.new
 puts "creating a new Watir browser"
 i =0
 while i < 9 do
+  puts "going to event page"
   browser.goto 'http://www.axs.com'
   browser.link(:class => "events_link", :index => i).when_present.click
   if browser.div(:class => "column-secondary").exists? 
     #print out the stuff
     event_id = browser.input(:id=> "event_id").value
     j= 0 
-#while browser.link(:class => "btn-size-small",  :index => j).exists? do 
-     
+    puts "printing out event ticket options" 
     while browser.div(:class => "call-to-action",  :index => j).exists? do 
         text = browser.link(:class => "btn-size-small", :index => j).text
         puts "Purchase button " + j.to_s + " for event " + event_id.to_s + ": " + text
@@ -29,15 +29,18 @@ end
 browser.goto 'http://www.axs.com'
 #text = browser.link(:class => "events_link", :index => i).html
 #puts text
+puts "going to event page"
 browser.link(:class => "events_link", :index => 20).when_present.click
 if browser.div(:class => "column-secondary").exists? 
   #print out the stuff
   event_id = browser.input(:id=> "event_id").value
-  j = 0;
+  j = 0
+  puts "printing out event ticket options"
   while browser.div(:class => "call-to-action",  :index => j).exists? do 
     text = browser.link(:class => "btn-size-small", :index => j).text
     puts "Purchase button " + j.to_s + " for event " + event_id.to_s + ": " + text
     j += 1
   end
 end
+puts "TEST PASS"
 browser.close
